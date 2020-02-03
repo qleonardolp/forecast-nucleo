c15f2f713c3d454fb6ee3e73cc49e7941f1dd858
Noè Murr <nosba95@yahoo.com>
2020-02-03 17:25:17 +0100

    [BACKUP]

--- src/main.cpp
+++ src/main.cpp
@@ -2,51 +2,20 @@
 #include <mbed.h>
 
 int main() {
-    
+    DigitalOut led(LED1);
     forecast::App app;
 
-    app.setLogger([](const forecast::Hardware*, const control::Control*,
+    float d = 12.0f;
+
+    app.setLogger([&](const forecast::Hardware*, const control::Control*,
                      const control::Control*) {
-        return std::vector<float>{6.f, 1.f};
+        return std::vector<float>{6.f, 1.f, d};
     });
 
-
+    d = app.requireFloatValue("hello");
+    
     while (true) {
         app.logInfo();
-        wait(0.5);
-        app.logError("hello from mbed!");
-        wait(0.5);
     }
+
 }
-
-// #include <USART/USART_interface.hpp>
-// USART_interface pc(USBTX,
-//                    USBRX,
-//                    C_8bits,
-//                    C_Use_RX_TX,
-//                    921600,
-//                    C_Parity_None,
-//                    C_1stop,
-//                    C_RX_TX_DMA,
-//                    100,
-//                    100);
-
-// int main() {
-//     pc.enable();
-//     pc.receive_enable();
-//     pc.transmit_enable();
-
-//     // uint8_t bytes[] = {0x02, 0x00, 0x00, 0x00, 0x80, 0x3f,
-//     //                    0x00, 0x00, 0x00, 0x40, 0x03};
-
-//     // while (true) {
-//     //     pc.put_tx_buffer(11, bytes);
-//     //     pc.send(false);
-//     //     wait(1);
-//     // }
-//     size_t i = 0;
-//     while(true) {
-//         pc.printf("test %u\r\n", i++);
-//         wait(1);
-//     }
-// }
