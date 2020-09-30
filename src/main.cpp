94f9df3c8c5653a5097ea91b8bfad5283775917b
Noè Murr <nosba95@yahoo.com>
2020-09-30 23:36:23 +0200

    [REFACTOR] renaming to new notation

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,25 @@
 #include <mbed.h>
+
 #include <debug.hpp>
 #include <forecast/App.hpp>
 #include <forecast/controllers/ForcePID.hpp>
 #include <forecast/controllers/ImpedanceControl.hpp>
 #include <forecast/controllers/MegaPD.hpp>
-
 #include <memory>
 
 int main() {
-
     forecast::App app;
-    app.setLogger([](const forecast::Hardware* hw, const forecast::Controller*,
-                     const forecast::Controller*) {
-
-        return std::vector<float>{ hw->getTauS().val, hw->getTauM().val} ; 
+    app.setLogger([](utility::ddvar, const forecast::Hardware* hw,
+                     const forecast::Controller*, const forecast::Controller*) {
+        return std::vector<float>{hw->getTauS(), hw->getTauM()};
     });
 
-    app.setEnvRefGen([](const forecast::Hardware* hw) { 
-            utility::ddvar test;
-            test.val = 0.0f;
-            test.dval = 0.0f;
-            test.ddval = 0.0f;
-            return test;
+    app.setEnvRefGen([](const forecast::Hardware* hw) {
+        utility::ddvar test;
+        test.val = 0.0f;
+        test.dval = 0.0f;
+        test.ddval = 0.0f;
+        return test;
     });
 
     app.setMotorRefGen([](const forecast::Hardware* hw) {
@@ -31,7 +29,7 @@ int main() {
         test.ddval = 0.0f;
         return test;
     });
-    
+
     app.setMotor(new forecast::ForcePID);
     // app.setEnviorment(new forecast::ForcePID);
     app.setEnviorment(new forecast::ImpedanceControl);
@@ -45,7 +43,6 @@ int main() {
     auto freq = app.requireFloatValue("frequency of the loop");
 
     app.execControlLoop(static_cast<ulong>(freq));
-    // app.execControlLoop(3000);
 }
 
 // #include <forecast/debug.hpp>
