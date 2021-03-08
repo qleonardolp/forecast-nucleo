caf287cd3fe8eb54de6b438727270bef8cd872da
rudyvic <visotruce@gmail.com>
2021-03-08 14:33:21 +0100

    update to last ForecastNucleoFramework

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,79 @@
 #include <mbed.h>
 #include <debug.hpp>
 #include <forecast/App.hpp>
-#include <forecast/controllers/ImpedanceControl.hpp>
 #include <forecast/controllers/ForcePID.hpp>
-#include <forecast/controllers/TransparencyControl.hpp>
-
-
+#include <forecast/controllers/PositionPID.hpp>
+#include <forecast/controllers/ImpedanceControl.hpp>
+#include <forecast/controllers/AdmittanceControl.hpp>
+#include <forecast/controllers/EnvRenderingControl.hpp>
 #include <memory>
+
+void setInertiaTask(forecast::App &app);
 
 int main() {
     forecast::App app;
+    
+    // set the task
+    setInertiaTask(app);
+
+    // Handshake with the PC
+    app.waitConnection();
+
+    // Motor Reference
+    // app.setMotorRefSweep();
+
+    // Require parameters for the controllers which are not already initialized
+    app.requireMotorParams();
+    app.requireEnvironmentParams();
+
+    // Require the loop frequency
+    auto freq = app.requireFloatValue("Loop frequency");
+
+    // Execute control loop
+    app.execControlLoop(static_cast<ulong>(freq));
+}
+
+void setInertiaTask(forecast::App &app) {
+    // Pick which values to log (time is logged as first value automatically)
     app.setLogger([](float envRef, const forecast::Hardware* hw, 
         const forecast::Controller* motor, forecast::Controller* env) {
-        // forecast::ImpedanceControl* c = static_cast<forecast::ImpedanceControl*>(env);
-
-        // float err = 0 - hw->getThetaE().val;
-        // float derr = hw->getThetaE().dval;
-      
-        // float tau_ref = c->k_des * err + c->b_des * derr;
-
         return std::vector<float>{
-            hw->getT(),
             hw->getTauM(),
-            // hw->control_motor->getTorqueFeedback(),
-            // -hw->getTauS(),diff(dtheta_m)/ts
             // envRef,
             hw->getThetaM(),
-            hw->getThetaE(),
-            // hw->getDThetaM(),
+            hw->getDThetaM(),
             // hw->getDDThetaM(),
+            // hw->getTauSensor(),
+            // hw->getTauE(),
+            // hw->getThetaE(),
+            // hw->getDThetaE(),
+            // hw->getDDThetaE(),
+            // hw->getTauSensor(),
+            // hw->getTauS(),
+            // hw->getDDThetaE(),
         }; 
     });
 
+    // Hard-coded reference for the environment
     app.setEnvRefGen([](const forecast::Hardware* hw) { 
         float test = 0;
-        // float ts = 0.0005f;
-        // static float t = 0.0;
+        static float t = 0.0;
 
-        // t += 0.25*0.0005f;
-        // if(t <= 1.0f){
-        //     t = 1.0f;
+        // STEP
+        // if(t >= 1.0f){
+        //     test = .5f;
         // }
-        // test = 0.2*sin(2.0*M_PI*0.5*t);
-        // t += ts;
+
+        // RAMP
+        // static float ramp = 0.0f;
+        // ramp += 0.3 * hw->getDT();
+        // if(ramp >= 1.5f){
+        //     ramp = 1.5f;
+        // }
+        // test = ramp;
+
+        // // SIN
+        // test = sin(2.0*M_PI*0.5*t);
 
         // // SWEEP GEN 
         // float sweep_amplitude = 0.2f;
@@ -51,16 +82,17 @@ int main() {
         // float m = sweep_max_freq / sweep_duration;
         // float rect = (m/2.0)*t;
         // if(t<=sweep_duration) {
-        //     t += ts;
+        //     t += hw->DT;
         // }
-        // test = sweep_amplitude * sin(2.0*M_PI*t*rect); 
-        // END 
+        // test = sweep_amplitude * sin(2.0*M_PI*t*rect);
         
+        t += hw->getDT();
         return test;
     });
 
+    // Hard-coded reference for the motor
     app.setMotorRefGen([](const forecast::Hardware* hw) {
-        float test = .0f;
+        float test = 0;
         static float t = 0.0;
 
         // // STEP
@@ -68,88 +100,36 @@ int main() {
         //     test = 1.0f;
         // }
 
-        // // RAMP
-        // static float ramp = 0.0f;
-        // ramp += 0.15 * hw->getDT();
-        // if(ramp >= 0.5f){
-        //     ramp = 0.5f;
-        // }
-        // test = ramp;
+        // RAMP
+        static float ramp = 0.0f;
+        ramp += 0.4 * hw->getDT();
+        if(ramp >= 3.5f){
+            ramp = 3.5f;
+        }
+        test = ramp;
 
         // SIN
-        // if(t <= 1.0f){
-            // t = 1.0f;
+        // test = 3*sin(2.0*M_PI*0.3*t);
+
+        // // SWEEP GEN 
+        // float sweep_amplitude = 0.2f;
+        // float sweep_duration = 20.0f;
+        // float sweep_max_freq = 10.0f;
+        // float m = sweep_max_freq / sweep_duration;
+        // float rect = (m/2.0)*t;
+        // if(t<=sweep_duration) {
+        //     t += hw->DT;
         // }
-        // test = sin(2.0*M_PI*1.0*t);
-
-        // float amplitude = 0.2;
-        // float time = hw->getT();
-        // static float startTime = 0.0f; 
-        // static float startTime2 = 0.0f;
-
-        // int t = 5;
-
-        // if(startTime == 0)
-        //     startTime = time;
-        // if(time - startTime < 4*t) {
-        //     float stepa;
-        //     if(time - startTime < t)
-        //         stepa = amplitude;
-        //     else if(time - startTime < 2*t)
-        //         stepa = 3*amplitude;
-        //     else if(time - startTime < 3*t)
-        //         stepa = 4*amplitude;
-        //     else
-        //         stepa = 2*amplitude;    
-        //     test.val = stepa;
-        //     test.dval = 0.0f;
-        //     test.ddval = 0.0f;
-        // } else {
-        //     if(startTime2 == 0)
-        //         startTime2 = time;
-        //     float freq_max = 10.0f;
-        //     float duration = 15.0f;
-        //     float m = freq_max/duration;
-        //     float freq = (m/2)*(time-startTime2);
-
-        //     test.val = 2*amplitude+amplitude*sin(2*M_PI*freq*(time-startTime2));
-        //     test.dval = 0.0f;
-        //     test.ddval = 0.0f;
-        //     if(time-startTime2 >= duration) {
-        //         test.val = 0.0f;
-        //         test.dval = 0.0f;
-        //         test.ddval = 0.0f;
-        //     }
-        // }
-
-        // static float t = 0;
-        // t += 0.00001f;
-        // if(t >= 1.0f){
-        //     t = 1.0f;
-        // }
-        // test = t;
+        // test = sweep_amplitude * sin(2.0*M_PI*t*rect);
 
         t += hw->getDT();
-        
         return test;
     });
     
+    // Motor controller
     app.setMotor(new forecast::ForcePID);
-    app.setEnviorment(new forecast::ImpedanceControl);
 
-    app.waitConnection();
+    // Environment controller
+    app.setEnvironment(new forecast::EnvRenderingControl);
 
-    app.requireMotorParams();
-    app.requireEnvironmentParams();
-
-    auto freq = app.requireFloatValue("frequency of the loop");
-
-    app.execControlLoop(static_cast<ulong>(freq));
-}
-
-// #include <forecast/debug.hpp>
-// int main() {
-//     DEBUG_INFO("0123456789\r\n");
-//     // Serial debug(PC_10, PC_11, 9600);
-//     // debug.printf("0123456789\r\n");
-// }
+} 
